a80e5d439ef2e5fcf60602bf3a8e019968ee0cd0
Ankita Lal <91656404+Ankita-lal@users.noreply.github.com>
2024-08-05 11:36:55 +0530

    Add files via upload
    Right-Angled Number Pyramid Pattern Solution added

---
+++ Pattern3Sol.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void printTriangle(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                cout<<j<<" ";
+            }
+            cout<<endl;
+        }
+}
+int main(){
+    int n;
+    cin>>n;
+    printTriangle(n);
+    return 0;
+}
+
