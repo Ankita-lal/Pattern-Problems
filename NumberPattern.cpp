a644bdd47e2d980828ded71608a12e23c1e0edfd
Ankita Lal <91656404+Ankita-lal@users.noreply.github.com>
2024-08-06 19:34:34 +0530

    Solution updated.
    One of the most difficult pattern problem.

---
+++ NumberPattern.cpp
@@ -0,0 +1,52 @@
+Pattern - 22: The Number Pattern
+
+Problem Statement: Given an integer N, print the following pattern : 
+
+
+Examples:
+
+Input Format: N = 3
+Result: 
+3 3 3 3 3 
+3 2 2 2 3 
+3 2 1 2 3 
+3 2 2 2 3 
+3 3 3 3 3
+
+Input Format: N = 6
+Result:   
+6 6 6 6 6 6 6 6 6 6 6 
+6 5 5 5 5 5 5 5 5 5 6 
+6 5 4 4 4 4 4 4 4 5 6 
+6 5 4 3 3 3 3 3 4 5 6 
+6 5 4 3 2 2 2 3 4 5 6 
+6 5 4 3 2 1 2 3 4 5 6 
+6 5 4 3 2 2 2 3 4 5 6 
+6 5 4 3 3 3 3 3 4 5 6 
+6 5 4 4 4 4 4 4 4 5 6 
+6 5 5 5 5 5 5 5 5 5 6 
+6 6 6 6 6 6 6 6 6 6 6
+
+
+void getNumberPattern(int n) {
+    // Write your code here.
+    for(int i=0;i<2*n-1;i++){
+         
+         
+         for(int j=0;j<2*n-1;j++){
+             
+             // Initialising the top, down, left and right indices of a cell.
+             int top = i;
+             int bottom = j;
+             int right = (2*n - 2) - j;
+             int left = (2*n - 2) - i;
+             
+             
+             cout<<(n- min(min(top,bottom), min(left,right)));
+         }
+         
+         
+         cout<<endl;
+     }
+    
+}
